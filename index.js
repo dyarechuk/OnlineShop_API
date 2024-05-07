e6bee192264363afb17d90bfb7b8cedab5a008f8
Dmytro Yarechuk <retrodn14@gmail.com>
2024-05-07 16:01:05 +0300

    changed port

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get('/products', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
